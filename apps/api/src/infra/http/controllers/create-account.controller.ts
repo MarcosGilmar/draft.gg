23f326db6d750135e2fbbdb553049deb56cad23c
marcosgilmar <marcosgilmarvalentini@gmail.com>
2026-05-07 15:33:25 -0300

    fix(api): adjust account creation controller to authenticate user and return access_token

--- apps/api/src/infra/http/controllers/create-account.controller.ts
+++ apps/api/src/infra/http/controllers/create-account.controller.ts
@@ -1,15 +1,27 @@
-import { Body, Controller, HttpCode, Post, UsePipes } from '@nestjs/common';
+import {
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+  UnauthorizedException,
+  UsePipes,
+} from '@nestjs/common';
 import {
   type CreateAccountBodySchema,
   createAccountBodySchema,
 } from '@repo/shared/schemas/auth';
+import { AuthenticateUserUseCase } from 'src/domain/use-cases/authenticate-user';
 import { CreateUserUseCase } from 'src/domain/use-cases/create-user';
 import { Public } from 'src/infra/auth/public';
 import { ZodValidationPipe } from 'src/infra/http/pipes/zod-validation-pipe';
 
 @Controller('/accounts')
 export class CreateAccountController {
-  constructor(private readonly createUser: CreateUserUseCase) {}
+  constructor(
+    private readonly createUser: CreateUserUseCase,
+    private readonly authenticateUser: AuthenticateUserUseCase,
+  ) {}
 
   @Post()
   @Public()
@@ -18,6 +30,27 @@ export class CreateAccountController {
   async handle(@Body() body: CreateAccountBodySchema) {
     const { name, email, password } = body;
 
-    await this.createUser.execute({ name, email, password });
+    const createUserResult = await this.createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    if (createUserResult.isLeft()) {
+      throw new ConflictException();
+    }
+
+    const authenticateUserResult = await this.authenticateUser.execute({
+      email,
+      password,
+    });
+
+    if (authenticateUserResult.isLeft()) {
+      throw new UnauthorizedException();
+    }
+
+    const { accessToken } = authenticateUserResult.value;
+
+    return { access_token: accessToken };
   }
 }
